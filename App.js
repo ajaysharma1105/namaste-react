3e8f1770693e9c66ed7c45fb49d7d8fc34fed912
AJay Sharma <ajaysharma1105@gmail.com>
2025-04-21 20:34:32 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     {id:"parent"},
     [React.createElement(
         "div",
         {id:"child"}
-    ,[ React.createElement("h1",{},"I'm an h1 tag") ,React.createElement("h2",{},"I'm an h2 tag")]
+    ,[ React.createElement("h1",{},"This is Namaste React :) " ) ,React.createElement("h2",{},"I'm an h2 tag")]
 ),
 React.createElement(
     "div",
@@ -17,6 +20,6 @@ React.createElement(
 
 const heading = React.createElement(
     "h2",{id:"heading",xyz :"abc"},"Hello World from React!");
-    const root2 = ReactDOM.createRoot(document.getElementById("root"));
-    root2.render(heading);
-    root2.render(parent);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
+    root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel
+- Dev build
+- Local Server
+- HMR = Hot module Replacement
+- keeps an eye on all file - File Watching Algorithm (written C++) 
+- Caching - Faster Builds
+- image optimization
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browser
+- Error handling
+- Tree shaking - remove unused code
+- Different dev and prod bundles
+
+
+  
